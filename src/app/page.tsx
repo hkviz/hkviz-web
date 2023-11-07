9534e814976adf4da424217d12e97e7bc1b4879b
Oliver Grack <oliver.grack@gmail.com>
2023-11-07 01:23:52 +0100

    commented out latest post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,15 +66,15 @@ async function CrudShowcase() {
   const session = await getServerAuthSession();
   if (!session?.user) return null;
 
-  const latestPost = await api.post.getLatest.query();
+  // const latestPost = await api.post.getLatest.query();
 
   return (
     <div className="w-full max-w-xs">
-      {latestPost ? (
+      {/* {latestPost ? (
         <p className="truncate">Your most recent post: {latestPost.name}</p>
       ) : (
         <p>You have no posts yet.</p>
-      )}
+      )} */}
 
       <CreatePost />
     </div>
