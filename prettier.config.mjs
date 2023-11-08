8b4afc831e78d5967726fd8a307e66939ed365fc
Oliver Grack <oliver.grack@gmail.com>
2023-11-08 20:04:52 +0100

    shadcn + basic menu ui

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+	trailingComma: 'all',
+	tabWidth: 4,
+	printWidth: 120,
+	semi: true,
+	singleQuote: true,
+	plugins: ['prettier-plugin-tailwindcss'],
+}
 
-export default config;
+export default config
