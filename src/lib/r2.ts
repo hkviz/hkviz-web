d3f8fe4930630695f55312c9cc716667f0cfaed3
Oliver Gstöttenbauer <oliver.gstoettenbauer@cloudflight.io>
2023-11-09 01:19:17 +0100

    uploading to r2

---
+++ src/lib/r2.ts
@@ -0,0 +1,13 @@
+import { S3Client } from '@aws-sdk/client-s3';
+import { env } from '~/env.mjs';
+
+console.log(env);
+export const r2 = new S3Client({
+    region: 'auto',
+    endpoint: env.R2_ENDPOINT,
+    credentials: {
+        accessKeyId: env.R2_ACCESS_KEY_ID,
+        secretAccessKey: env.R2_SECRET_ACCESS_KEY,
+    },
+});
+console.log(r2.config);
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
-import { postRouter } from "~/server/api/routers/post";
-import { createTRPCRouter } from "~/server/api/trpc";
+import { postRouter } from '~/server/api/routers/post';
+import { createTRPCRouter } from '~/server/api/trpc';
+import { uploadRouter } from './routers/upload';
 
 /**
  * This is the primary router for your server.
@@ -7,7 +8,8 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+    post: postRouter,
+    upload: uploadRouter,
 });
 
 // export type definition of API
