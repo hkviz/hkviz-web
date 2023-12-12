d2ef9c7fd4b3c2f9ea390e68d69c32ea2d90179b
Oliver Grack <oliver.grack@gmail.com>
2023-12-12 01:21:14 +0100

    favicons generated

---
+++ generate-favicons.mjs
@@ -0,0 +1,29 @@
+import favicons from 'favicons';
+import fs from 'fs/promises';
+import path from 'path';
+
+const src = './logo/logo.png'; // Icon source file path.
+const dest = './public/favicons'; // Output directory path.
+const htmlBasename = 'index.html'; // HTML file basename.
+
+/** @type {import('favicons').FaviconOptions} */
+const configuration = {
+    path: '/favicons',
+    appName: 'HKViz',
+    appShortName: 'HKViz',
+    appDescription: 'Visual analytics for HollowKnight',
+    background: '#06111d',
+    theme_color: '#6245bc94',
+};
+
+// Below is the processing.
+const response = await favicons(src, configuration);
+await fs.mkdir(dest, { recursive: true });
+await Promise.all(
+    response.images.map(async (image) => await fs.writeFile(path.join(dest, image.name), image.contents)),
+);
+await Promise.all(response.files.map(async (file) => await fs.writeFile(path.join(dest, file.name), file.contents)));
+await fs.writeFile(path.join(dest, htmlBasename), response.html.join('\n'));
+
+await fs.rm('./public/favicon.ico');
+await fs.rename(path.join(dest, 'favicon.ico'), './public/favicon.ico');
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,15 @@ import '~/styles/globals.css';
 
 import { cookies } from 'next/headers';
 
+import { type Metadata } from 'next';
 import { getServerAuthSession } from '~/server/auth';
-import { cinzel, notoSans, cinzelDecorative } from '~/styles/fonts';
+import { cinzel, cinzelDecorative, notoSans } from '~/styles/fonts';
 import { TRPCReactProvider } from '~/trpc/react';
 import ClientContext from './_components/context';
 import { MainNav } from './_components/main-nav';
+import { FaviconsHead } from './_favicons-head';
 
-export const metadata = {
+export const metadata: Metadata = {
     title: 'HKViz for Hollow Knight',
     description: 'Visual Analytics for HollowKnight',
     icons: [{ rel: 'icon', url: '/favicon.ico' }],
@@ -19,6 +21,9 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
     return (
         <html lang="en">
+            <head>
+                <FaviconsHead />
+            </head>
             <body className={`dark font-sans ${notoSans.variable} ${cinzel.variable} ${cinzelDecorative.variable}`}>
                 <ClientContext>
                     <TRPCReactProvider cookies={cookies().toString()}>
