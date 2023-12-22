bc881f910574140c7d23cbc9c7e49da783ac324e
Oliver Grack <oliver.grack@gmail.com>
2023-12-22 21:47:41 +0100

    mdx consent page wrapped in react server component

---
+++ src/app/study-consent-data-usage/page.tsx
@@ -0,0 +1,44 @@
+import { apiFromServer } from '~/trpc/from-server';
+import { DataCollectionStudyParticipationClientForm } from './_client_components';
+import { cookies } from 'next/headers';
+import { getServerAuthSession } from '~/server/auth';
+import { AuthNeeded } from '../_components/auth-needed';
+import { MdxInnerWrapper, MdxOuterWrapper } from '../_components/mdx-layout';
+import MdxContent from './_page.mdx';
+
+export async function DataCollectionStudyParticipationForm({
+    className,
+    formPositionText,
+}: {
+    className?: string;
+    formPositionText: string;
+}) {
+    const studyParticipation = await (await apiFromServer()).studyParticipation.getStudyParticipation({});
+    const hasIngameAuthCookie = cookies().get('ingameAuthUrlId') != null;
+    return (
+        <DataCollectionStudyParticipationClientForm
+            savedStudyParticipation={studyParticipation}
+            formPositionText={formPositionText}
+            className={className}
+            hasIngameAuthCookie={hasIngameAuthCookie}
+        />
+    );
+}
+
+export default async function DataCollectionStudyParticipationPage() {
+    const session = await getServerAuthSession();
+
+    if (!session) {
+        return <AuthNeeded />;
+    }
+
+    return (
+        <MdxOuterWrapper>
+            <DataCollectionStudyParticipationForm formPositionText="bellow" className="mx-auto my-[10rem]" />
+            <MdxInnerWrapper>
+                <MdxContent />
+            </MdxInnerWrapper>
+            <DataCollectionStudyParticipationForm className="mx-auto mb-[20rem] mt-[10rem]" formPositionText="above" />
+        </MdxOuterWrapper>
+    );
+}
