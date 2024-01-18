3440d773f746cd356dca6d3a9f4a6a58fd0e948c
Oliver Grack <oliver.grack@gmail.com>
2024-01-18 18:27:06 +0100

    mod version check url

---
+++ src/app/api/rest/modversioncheck/[version]/route.tsx
@@ -0,0 +1,52 @@
+import { typeCheckNever } from '~/lib/utils/utils';
+import { type ModVersion as HKVizModVersion } from '~/lib/viz/types/recording-file-version';
+
+interface ModVersionCheckResponse {
+    message: string;
+    color: 'red' | 'blue' | 'cyan' | 'white' | 'green';
+    show: boolean;
+}
+
+function getVersionCheckResult(version: string): ModVersionCheckResponse {
+    const versionTyped = version as HKVizModVersion;
+    const toUpdate = 'To update, close HollowKnight and open a mod manager like Lumafly or Scarab.';
+
+    console.log(version);
+    switch (versionTyped) {
+        case '1.5.0.0':
+            return {
+                message: '',
+                color: 'cyan',
+                show: false,
+            };
+        case '1.3.0.0':
+        case '1.4.0.0':
+            return {
+                message: `A newer version of HKViz has been released. ${toUpdate}`,
+                color: 'cyan',
+                show: true,
+            };
+
+        case '0.0.0.0':
+        case '1.0.0.0':
+        case '1.1.0.0':
+        case '1.2.0.0':
+            return {
+                message: `You are using a old version of HKViz. ${toUpdate}`,
+                color: 'red',
+                show: true,
+            };
+        default: {
+            typeCheckNever(versionTyped);
+            return {
+                message: `You are using a unknown version of HKViz. ${toUpdate}`,
+                color: 'cyan',
+                show: true,
+            };
+        }
+    }
+}
+
+export function GET(request: Request, { params }: { params: { version: string } }) {
+    return Response.json(getVersionCheckResult(params.version));
+}
--- src/lib/viz/types/recording-file-version.ts
+++ src/lib/viz/types/recording-file-version.ts
@@ -1,5 +1,16 @@
 export const recordingFileVersions = [
     /**
+     * Changes in format:
+     * - writes mod info as name:version:enabled_errorenum instead of name:[version or error] gotten from the mod loader display string.
+     * - also fixes that mods are not written when to many are installed.
+     *
+     * Other mod changes:
+     * - fixes decoration master not loading
+     * - checks mod version when loading
+     */
+    '1.5.0',
+
+    /**
      * - writes hero state .transitioning
      * - writes info when HeroController.TakeDamage is called, aka. about how much damage, and who deals it
      * - writes info when PlayerData.TakeHealth is called, after hc.TakeDamage is called, which contains the final health value
@@ -29,6 +40,9 @@ export const recordingFileVersions = [
 ] as const;
 
 export type RecordingFileVersion = (typeof recordingFileVersions)[number];
+export type ModVersion = `${RecordingFileVersion}.0` | '1.2.0.0' | '1.3.0.0';
+
+export const newestModVersion: ModVersion = '1.5.0.0';
 
 export function isKnownRecordingFileVersion(version: string): version is RecordingFileVersion {
     return recordingFileVersions.includes(version as RecordingFileVersion);
@@ -39,11 +53,13 @@ export function isVersion0xx(version: RecordingFileVersion): version is Recordin
     return version === '0.0.0';
 }
 
-export type RecordingFileVersion1xx = '1.0.0' | '1.1.0' | '1.4.0';
+export type RecordingFileVersion1xx = '1.0.0' | '1.1.0' | '1.4.0' | '1.5.0';
 export function isVersion1xx(version: RecordingFileVersion): version is RecordingFileVersion1xx {
-    return version === '1.0.0' || version === '1.1.0' || version === '1.4.0';
+    return version === '1.0.0' || version === '1.1.0' || version === '1.4.0' || version === '1.5.0';
 }
 
-export function isVersionBefore1_4_0(version: RecordingFileVersion): version is RecordingFileVersion0xx | '1.0.0' | '1.1.0' {
+export function isVersionBefore1_4_0(
+    version: RecordingFileVersion,
+): version is RecordingFileVersion0xx | '1.0.0' | '1.1.0' {
     return isVersion0xx(version) || version === '1.0.0' || version === '1.1.0';
 }
