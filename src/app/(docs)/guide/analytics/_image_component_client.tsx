17f1d4b7b021efff9c5449d43f4a0daf1ff9aa58
Oliver Grack <oliver.grack@gmail.com>
2024-03-13 02:45:15 +0100

    further docs progress

---
+++ src/app/(docs)/guide/analytics/_image_component_client.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { createContext, useContext, useMemo, useState } from 'react';
+
+export const ImageAreaShadowContext = createContext<{
+    set?: (positionClassName: string) => void;
+    unset?: (positionClassName: string) => void;
+}>({});
+
+export function ImageAreaShadow({ children }: { children: React.ReactNode }) {
+    const [positionClassName, setPositionClassName] = useState<string | null>(null);
+
+    function unsetPositionClassName(className: string) {
+        setPositionClassName((value) => (value === className ? null : value));
+    }
+
+    const contextValue = useMemo(() => ({ set: setPositionClassName, unset: unsetPositionClassName }), []);
+
+    return (
+        <ImageAreaShadowContext.Provider value={contextValue}>
+            <div
+                className={
+                    'pointer-events-none absolute rounded-md shadow-[0_0_0_100vmax_rgba(0,0,0,0.5)] transition-all duration-100 dark:shadow-[0_0_0_100vmax_rgba(255,255,255,0.35)] ' +
+                    (positionClassName ?? 'left-0 top-0 h-full w-full')
+                }
+            ></div>
+            {children}
+        </ImageAreaShadowContext.Provider>
+    );
+}
+
+export function ImageArea({
+    positionClassName,
+    children,
+    href,
+}: {
+    positionClassName: string;
+    children: React.ReactNode;
+    href: string;
+}) {
+    const context = useContext(ImageAreaShadowContext);
+
+    function handleHover() {
+        context.set?.(positionClassName);
+    }
+
+    function handleUnhover() {
+        context.unset?.(positionClassName);
+    }
+
+    return (
+        <Button
+            className={cn(
+                'absolute z-20 rounded-sm border-2 border-dashed border-transparent bg-transparent p-0 text-transparent no-underline drop-shadow-glow-md transition duration-200 ease-in-out hover:bg-transparent hover:bg-opacity-0 group-hover:border-white',
+                // 'absolute z-20 rounded-sm border-2 border-red-600 bg-transparent p-0 text-transparent no-underline drop-shadow-glow-md transition duration-200 ease-in-out hover:bg-transparent hover:bg-opacity-0 group-hover:text-red-600 group-hover:opacity-100 dark:border-red-600 dark:drop-shadow-2xl dark:group-hover:text-red-600',
+                positionClassName,
+            )}
+            onMouseEnter={handleHover}
+            onMouseLeave={handleUnhover}
+            asChild
+        >
+            <a href={href}>{children}</a>
+        </Button>
+    );
+}
--- src/app/(docs)/guide/analytics/_screenshots.tsx
+++ src/app/(docs)/guide/analytics/_screenshots.tsx
@@ -1,7 +1,6 @@
-import { Button } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
-import Image from 'next/image';
 import { HKVizText } from '~/app/_components/hkviz-text';
+import { ImageContainer, ThemedImage } from './_image_component';
+import { ImageArea, ImageAreaShadow } from './_image_component_client';
 
 import overviewDarkSrc from './screenshots/overview_dark.png';
 import overviewLightSrc from './screenshots/overview_light.png';
@@ -15,48 +14,23 @@ import tracesAnimatedLightSrc from './screenshots/traces_animated_light.png';
 import tracesNoneDarkSrc from './screenshots/traces_none_dark.png';
 import tracesNoneLightSrc from './screenshots/traces_none_light.png';
 
-type ThemedImageProps = {
-    srcLight: React.ComponentProps<typeof Image>['src'];
-    srcDark: React.ComponentProps<typeof Image>['src'];
-    className?: string;
-    alt: string;
-};
+import roomAnalyticsDarkSrc from './screenshots/room_analytics_dark.png';
+import roomAnalyticsLightSrc from './screenshots/room_analytics_light.png';
 
-function ThemedImage({ srcLight, srcDark, className, alt }: ThemedImageProps) {
-    return (
-        <>
-            <Image alt={alt} src={srcLight} className={cn('block dark:hidden', className)} />
-            <Image alt={alt} src={srcDark} className={cn('hidden dark:block', className)} />
-        </>
-    );
-}
+import colorModeAreaDarkSrc from './screenshots/room_colors_area_dark.png';
+import colorModeAreaDarkTableSrc from './screenshots/room_colors_area_dark_table.png';
+import colorModeAreaLightSrc from './screenshots/room_colors_area_light.png';
+import colorModeAreaLightTableSrc from './screenshots/room_colors_area_light_table.png';
 
-function ImageContainer({ children, caption }: { children: React.ReactNode; caption?: React.ReactNode }) {
-    return (
-        <div>
-            <div className="group relative overflow-hidden rounded-md">{children}</div>
-            {caption && <ImageCaption>{caption}</ImageCaption>}
-        </div>
-    );
-}
+import colorModeLinearDarkSrc from './screenshots/room_colors_linear_dark.png';
+import colorModeLinearDarkTableSrc from './screenshots/room_colors_linear_dark_table.png';
+import colorModeLinearLightSrc from './screenshots/room_colors_linear_light.png';
+import colorModeLinearLightTableSrc from './screenshots/room_colors_linear_light_table.png';
 
-function ImageArea({ className, children, href }: { className: string; children: React.ReactNode; href: string }) {
-    return (
-        <Button
-            className={cn(
-                'absolute z-20 rounded-sm border-2 border-red-600 bg-transparent p-0 text-transparent no-underline shadow-[0_0_0_100vmax_rgba(0,0,0,0)] drop-shadow-glow-md transition duration-200 ease-in-out hover:z-10 hover:bg-transparent hover:bg-opacity-0 hover:shadow-[0_0_0_100vmax_rgba(0,0,0,0.7)] group-hover:text-red-600 group-hover:opacity-100 dark:border-red-600 dark:drop-shadow-2xl dark:group-hover:text-red-600',
-                className,
-            )}
-            asChild
-        >
-            <a href={href}>{children}</a>
-        </Button>
-    );
-}
-
-function ImageCaption({ children }: { children: React.ReactNode }) {
-    return <div className="text-center text-sm">{children}</div>;
-}
+import colorModeExpDarkSrc from './screenshots/room_colors_exp3_dark.png';
+import colorModeExpDarkTableSrc from './screenshots/room_colors_exp3_dark_table.png';
+import colorModeExpLightSrc from './screenshots/room_colors_exp3_light.png';
+import colorModeExpLightTableSrc from './screenshots/room_colors_exp3_light_table.png';
 
 export function OverviewScreenshot() {
     return (
@@ -67,30 +41,41 @@ export function OverviewScreenshot() {
                 </>
             }
         >
-            <ThemedImage
-                srcLight={overviewLightSrc}
-                srcDark={overviewDarkSrc}
-                className="m-0"
-                alt="HKViz gameplay analytics page"
-            />
-            <ImageArea className="left-[1%] top-[9%] h-[32%] w-[28%]" href="#map-options">
-                Map options
-            </ImageArea>
-            <ImageArea className="left-[1%] top-[42%] h-[57%] w-[28%]" href="#room-analytics">
-                Room analytics
-            </ImageArea>
-            <ImageArea className="left-[30%] top-[9%] h-[83%] w-[38%]" href="#map">
-                Map
-            </ImageArea>
-            <ImageArea className="left-[30%] top-[93%] h-[6%] w-[38%]" href="#timeline">
-                Timeline
-            </ImageArea>
-            <ImageArea className="left-[68.5%] top-[9%] h-[90%] w-[31%]" href="#splits">
-                Splits
-            </ImageArea>
-            {/* <ImageArea className="left-[82%] top-[9%] h-[8%] w-[10%]" href="#time-charts">
-                Time charts
-            </ImageArea> */}
+            <ImageAreaShadow>
+                <ThemedImage
+                    srcLight={overviewLightSrc}
+                    srcDark={overviewDarkSrc}
+                    className="m-0"
+                    alt="HKViz gameplay analytics page"
+                />
+                {/* <ImageArea positionClassName="left-[1%] top-[9%] h-[32%] w-[28%]" href="#map-options">
+                    Map options
+                </ImageArea> */}
+                <ImageArea positionClassName="left-[1%] top-[16%] h-[8%] w-[28%]" href="#room-visibility">
+                    Room visibility
+                </ImageArea>
+                <ImageArea positionClassName="left-[1%] top-[24.5%] h-[8%] w-[28%]" href="#player-movement-and-traces">
+                    Traces
+                </ImageArea>
+                <ImageArea positionClassName="left-[1%] top-[33%] h-[8%] w-[28%]" href="#map-coloring-by-variables">
+                    Room colors
+                </ImageArea>
+                <ImageArea positionClassName="left-[1%] top-[42%] h-[57%] w-[28%]" href="#map-coloring-by-variables">
+                    Room analytics
+                </ImageArea>
+                <ImageArea positionClassName="left-[30%] top-[9%] h-[83%] w-[38%]" href="#map">
+                    Map
+                </ImageArea>
+                <ImageArea positionClassName="left-[30%] top-[93%] h-[6%] w-[38%]" href="#timeline">
+                    Timeline
+                </ImageArea>
+                <ImageArea positionClassName="left-[68.5%] top-[9%] h-[90%] w-[31%]" href="#splits">
+                    Splits
+                </ImageArea>
+                <ImageArea positionClassName="left-[82%] top-[9%] h-[8%] w-[10%]" href="#time-charts">
+                    Time charts
+                </ImageArea>
+            </ImageAreaShadow>
         </ImageContainer>
     );
 }
@@ -129,6 +114,104 @@ export function TracesNoneScreenshot() {
                 srcDark={tracesNoneDarkSrc}
                 className="m-0"
                 alt="A game map showing rooms of Forgotten Crossroads with no traces on top, displaying player movement."
+            />
+        </ImageContainer>
+    );
+}
+
+export function RoomAnalyticsScreenshot() {
+    return (
+        <ImageContainer
+            className="max-w-48 md:float-right md:ml-4"
+            caption={
+                <>
+                    <HKVizText /> Room analytics panel
+                </>
+            }
+        >
+            <ThemedImage
+                srcLight={roomAnalyticsLightSrc}
+                srcDark={roomAnalyticsDarkSrc}
+                className="m-0"
+                alt="A table showing various variables of a room selected on the map."
+            />
+        </ImageContainer>
+    );
+}
+
+export function ColorModeAreaScreenshot() {
+    return (
+        <ImageContainer>
+            <ThemedImage
+                srcLight={colorModeAreaLightSrc}
+                srcDark={colorModeAreaDarkSrc}
+                className="m-0"
+                alt="A map showing the area colors of the game rooms"
+            />
+        </ImageContainer>
+    );
+}
+
+export function ColorModeAreaTableScreenshot() {
+    return (
+        <ImageContainer className="mb-2">
+            <ThemedImage
+                srcLight={colorModeAreaLightTableSrc}
+                srcDark={colorModeAreaDarkTableSrc}
+                className="m-0"
+                alt="Variable table row showing area color mode selected"
+            />
+        </ImageContainer>
+    );
+}
+
+export function ColorModeLinearScreenshot() {
+    return (
+        <ImageContainer>
+            <ThemedImage
+                srcLight={colorModeLinearLightSrc}
+                srcDark={colorModeLinearDarkSrc}
+                className="m-0"
+                alt="A map showing rooms colored by the geo earned by area"
+            />
+        </ImageContainer>
+    );
+}
+
+export function ColorModeLinearTableScreenshot() {
+    return (
+        <ImageContainer className="mb-2">
+            <ThemedImage
+                srcLight={colorModeLinearLightTableSrc}
+                srcDark={colorModeLinearDarkTableSrc}
+                className="m-0"
+                alt="Variable table row showing linear color scale selected"
+            />
+        </ImageContainer>
+    );
+}
+
+export function ColorModeExpScreenshot() {
+    return (
+        <ImageContainer>
+            <ThemedImage
+                srcLight={colorModeExpLightSrc}
+                srcDark={colorModeExpDarkSrc}
+                className="m-0"
+                alt="A map showing rooms colored by the geo earned by area in an exponential scale"
+            />
+        </ImageContainer>
+    );
+}
+
+export function ColorModeExpTableScreenshot() {
+    return (
+        <ImageContainer className="mb-2">
+            <ThemedImage
+                srcLight={colorModeExpLightTableSrc}
+                srcDark={colorModeExpDarkTableSrc}
+                className="m-0"
+                alt="Variable table row showing exponential color scale selected"
             />
         </ImageContainer>
     );
