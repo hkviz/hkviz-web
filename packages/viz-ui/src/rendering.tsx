0ec3b0ad3906ea0bbba335d89d51d02d6552c6eb
Oliver Grack <oliver.grack@gmail.com>
2025-01-21 22:16:44 +0100

    Try fixing dynamic typed component

--- packages/viz-ui/src/rendering.tsx
+++ packages/viz-ui/src/rendering.tsx
@@ -2,9 +2,11 @@ import { type Component } from 'solid-js';
 import { Dynamic, render as solidRender, renderToString as renderToStringSolid } from 'solid-js/web';
 
 export function render<T>(element: Element, component: Component<T>, props: T) {
-    return solidRender(() => <Dynamic component={component} {...props} />, element);
+    const DynamicAny = Dynamic as any;
+    return solidRender(() => <DynamicAny component={component} {...props} />, element);
 }
 
 export function renderToString<T>(component: Component<T>, props: T) {
-    return renderToStringSolid(() => <Dynamic component={component} {...props} />);
+    const DynamicAny = Dynamic as any;
+    return renderToStringSolid(() => <DynamicAny component={component} {...props} />);
 }
