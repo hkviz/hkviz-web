5796e9884921722f9eb3ca5b0b2a43f4aa06140c
OliverGrack <oliver.grack@gmail.com>
2025-09-30 22:08:16 +0200

    tmp remove mail login + use worker mailer for sending mails

--- src/lib/auth/auth-options.tsx
+++ src/lib/auth/auth-options.tsx
@@ -1,6 +1,6 @@
 import DiscordProvider from '@auth/core/providers/discord';
 import GoogleProvider from '@auth/core/providers/google';
-import EmailProvider from '@auth/core/providers/nodemailer';
+// import EmailProvider from '@auth/core/providers/nodemailer';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { type SolidAuthConfig } from '@auth/solid-start';
 import { isValid as isValidNonBlacklistedEmail } from 'mailchecker';
@@ -41,17 +41,17 @@ export const authOptions: SolidAuthConfig = {
 			clientId: env.GOOGLE_CLIENT_ID,
 			clientSecret: env.GOOGLE_CLIENT_SECRET,
 		}),
-		EmailProvider({
-			server: {
-				host: env.EMAIL_SERVER_HOST,
-				port: env.EMAIL_SERVER_PORT,
-				auth: {
-					user: env.EMAIL_SERVER_USER,
-					pass: env.EMAIL_SERVER_PASSWORD,
-				},
-			},
-			from: env.EMAIL_FROM,
-		}),
+		// EmailProvider({
+		// 	server: {
+		// 		host: env.EMAIL_SERVER_HOST,
+		// 		port: env.EMAIL_SERVER_PORT,
+		// 		auth: {
+		// 			user: env.EMAIL_SERVER_USER,
+		// 			pass: env.EMAIL_SERVER_PASSWORD,
+		// 		},
+		// 	},
+		// 	from: env.EMAIL_FROM,
+		// }),
 	],
 	theme: {
 		logo: '/favicons/android-chrome-192x192.png',
--- src/server/mails.ts
+++ src/server/mails.ts
@@ -1,32 +1,73 @@
-import { createTransport } from 'nodemailer';
-import type Mail from 'nodemailer/lib/mailer';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport';
 import { env } from '~/env';
 
-const transporterOptions: SMTPTransport.Options = {
-	host: env.EMAIL_SERVER_HOST,
-	port: env.EMAIL_SERVER_PORT,
-	auth: {
-		user: env.EMAIL_SERVER_USER,
-		pass: env.EMAIL_SERVER_PASSWORD,
-	},
-	tls: {
-		ciphers: 'SSLv3',
-	},
-};
+interface SendMailOptions {
+	to: string;
+	from: string;
+	subject: string;
+	text: string;
+	html?: string;
+}
 
-export const mailTransporter = createTransport(transporterOptions);
+async function sendMail(options: SendMailOptions) {
+	  // Check if running in development environment
+  if ((import.meta as any).dev) {
+    // Development: nodemailer
+	  const nodemailer = await import('nodemailer');
 
-export function sendMailToSupport(options: Omit<Mail.Options, 'from' | 'to'>) {
-	return mailTransporter.sendMail({
+	  const transporterOptions: SMTPTransport.Options = {
+		host: env.EMAIL_SERVER_HOST,
+		port: env.EMAIL_SERVER_PORT,
+		auth: {
+			user: env.EMAIL_SERVER_USER,
+			pass: env.EMAIL_SERVER_PASSWORD,
+		},
+		tls: {
+			ciphers: 'SSLv3',
+		},
+	};
+
+	  const transporter = nodemailer.default.createTransport(transporterOptions);
+	  return await transporter.sendMail({
+		  from: options.from,
+		  to: options.to,
+		  subject: options.subject,
+		  text: options.text,
+		  html: options.html,
+	  });
+  } else {
+    // Production: worker-mailer
+    const { WorkerMailer } = await import('worker-mailer')
+	  const mailer = await WorkerMailer.connect({
+		  credentials: {
+			  username: env.EMAIL_SERVER_USER,
+			  password: env.EMAIL_SERVER_PASSWORD,
+		  },
+		  authType: 'plain',
+		  host: env.EMAIL_SERVER_HOST,
+		  port: env.EMAIL_SERVER_PORT,
+		  secure: true,
+	  });
+	  return await mailer.send({
+		  from: options.from,
+		  to: options.to,
+		  subject: options.subject,
+		  text: options.text,
+		  html: options.html,
+	  });
+  }
+}
+
+export function sendMailToSupport(options: Omit<SendMailOptions, 'from' | 'to'>) {
+	return sendMail({
 		...options,
 		from: `"HKViz Backend" <${env.EMAIL_FROM}>`,
 		to: `"HKViz Support" <${env.EMAIL_FROM}>`,
 	});
 }
 
-export function sendMailToUser(options: Omit<Mail.Options, 'from'>) {
-	return mailTransporter.sendMail({
+export function sendMailToUser(options: Omit<SendMailOptions, 'from'>) {
+	return sendMail({
 		...options,
 		from: `"HKViz Support" <${env.EMAIL_FROM}>`,
 	});
