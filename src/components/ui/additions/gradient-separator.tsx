a22970a4b20308e8d8209c3aeecef1154eb70c3d
OliverGrack <oliver.grack@gmail.com>
2025-10-05 22:58:29 +0200

    hr style fix

--- src/components/ui/additions/gradient-separator.tsx
+++ src/components/ui/additions/gradient-separator.tsx
@@ -1,12 +1,12 @@
-import { cn } from '~/lib/utils';
 import { type Component } from 'solid-js';
+import { cn } from '~/lib/utils';
 import { Separator } from '../separator';
 
 export const GradientSeparator: Component<{ class?: string }> = (props) => {
 	return (
 		<Separator
 			class={cn(
-				'my-4 h-px border-none bg-transparent bg-linear-to-r from-transparent via-current to-transparent',
+				'my-4 h-px border-none bg-transparent bg-linear-to-r from-transparent via-foreground to-transparent',
 				props.class,
 			)}
 		/>
