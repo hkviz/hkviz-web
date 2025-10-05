8d564ee7c1cb584887ac8303c3b737f10ac68f03
OliverGrack <oliver.grack@gmail.com>
2025-10-05 22:49:35 +0200

    fix 2 more container margin spots

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
 	return (
 		<ContentCenterWrapper>
 			<Title>HKViz for Hollow Knight</Title>
-			<div class="container flex flex-col items-center justify-center gap-12 px-4 py-16 text-foreground">
+			<div class="container mx-auto flex flex-col items-center justify-center gap-12 px-4 py-16 text-foreground">
 				<div class={`max-w-[70ch] text-center`}>
 					<div class="start-page-header">
 						<h1
