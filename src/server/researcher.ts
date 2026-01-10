54ca5f9de47ceaa837e8677f4ffd8496d9d2087f
OliverGrack <oliver.grack@gmail.com>
2026-01-10 18:52:56 +0100

    fix broken link in credits

--- src/server/researcher.ts
+++ src/server/researcher.ts
@@ -3,29 +3,29 @@ import { type DB } from '~/server/db';
 import { users } from '~/server/db/schema';
 
 export async function isResearcher({ db, userId }: { db: DB; userId: string }) {
-    const sqlStr = sql<string>;
+	const sqlStr = sql<string>;
 
-    const result = await db
-        .select({
-            count: sqlStr`COUNT(*)`,
-        })
-        .from(users)
-        .where(and(eq(users.id, userId), eq(users.isResearcher, true)));
+	const result = await db
+		.select({
+			count: sqlStr`COUNT(*)`,
+		})
+		.from(users)
+		.where(and(eq(users.id, userId), eq(users.isResearcher, true)));
 
-    return result[0]?.count === '1';
+	return result[0]?.count?.toString() === '1';
 }
 
 export async function assertIsResearcher({
-    db,
-    userId,
-    makeError = () => new Error('Forbidden'),
+	db,
+	userId,
+	makeError = () => new Error('Forbidden'),
 }: {
-    db: DB;
-    userId: string | null;
-    makeError?: () => Error;
+	db: DB;
+	userId: string | null;
+	makeError?: () => Error;
 }) {
-    if (!userId) throw makeError();
-    if (!(await isResearcher({ db, userId }))) {
-        throw makeError();
-    }
+	if (!userId) throw makeError();
+	if (!(await isResearcher({ db, userId }))) {
+		throw makeError();
+	}
 }
