cd64a5cadecbeeda6554da1e7e616de13070e584
OliverGrack <oliver.grack@gmail.com>
2026-02-03 12:49:11 +0100

    very revolutionary glow added to player title in player page

--- src/routes/player/[id].tsx
+++ src/routes/player/[id].tsx
@@ -34,8 +34,8 @@ export default function PublicPlayerPage(props: { params: Params }) {
 						when={player()}
 						fallback={<p class="text-center">This player does not exist or has no public gameplays</p>}
 					>
-						<h2 class="text-center font-serif text-xl">Gameplays of</h2>
-						<h1 class="first-letter:font-serifDecorative mb-4 pl-2 text-center font-serif text-4xl font-semibold">
+						<h2 class="glowing-text-md text-center font-serif text-xl">Gameplays of</h2>
+						<h1 class="first-letter:font-serifDecorative glowing-text-md mb-4 pl-2 text-center font-serif text-4xl font-semibold">
 							{userName()}
 						</h1>
 						<RunFilters searchParams={filter()} class="mb-4" />
